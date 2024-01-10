ff3915b5d96ddb98d7928cd2d4724baa5acae422
tatin <tatenvader123@gmail.com>
2024-01-10 20:53:58 +0800

    login page

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-layout class="main" view="hHh lpR fFf">
+
+    <q-header bordered class="bg-white text-white">
+      <q-toolbar>
+        <q-toolbar-title class="text-center">
+          <img class="q-mt-sm" width="120px" src="../img/logo.png">
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script setup>
+
+
+</script>
+
+
+<style>
+.main {
+  background-image: url("src/img/gray-bg.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
---
+++ src/pages/AuthPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-page class="main" padding>
+    <div class="q-pa-md row q-mt-xl justify-center">
+      <form>
+        <q-card style="width: 330px;" class="col-auto text-center">
+          <q-card-section>
+            <img width="90" src="../img/logo.png" alt="">
+          </q-card-section>
+          <q-separator />
+          <q-card-section class="q-pa-lg">
+            <q-input class="q-mb-md" filled label="Email" />
+            <q-input type="password" filled label="Password" />
+          </q-card-section>
+          <q-separator />
+          <q-card-action>
+            <q-btn class="q-ma-xl q-px-xl" label="Log In" color="red-8" /> </q-card-action>
+        </q-card>
+      </form>
+    </div>
+    <!-- MODAL FOR SUCCESSFULLY DELETING THE LAB -->
+    <q-dialog seamless position="bottom">
+      <q-card class="bg-red-9" style="width: 350px">
+        <q-card-section class="row items-center no-wrap">
+          <div>
+            <div class="text-white">Invalid Credentials!</div>
+          </div>
+          <q-space />
+          <q-btn color="white" flat round icon="close" v-close-popup />
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </q-page>
+</template>
+
+
