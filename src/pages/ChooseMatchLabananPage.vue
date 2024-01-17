b8838230740541f08bed06e4f88fcbba60c29192
tatin <tatenvader123@gmail.com>
2024-01-17 12:46:17 +0800

    pairing up players updated

--- src/pages/ChooseMatchLabananPage.vue
+++ src/pages/ChooseMatchLabananPage.vue
@@ -31,7 +31,7 @@
       </div>
       <div class="col col-md-6 col-xs-12 q-pa-xs">
         <div class="text-center text-h6 q-mb-md text-bold">Choose Players</div>
-        <q-table :rows="rows" :columns="columns" row-key="name">
+        <q-table :rows="rows" :columns="columns" row-key="name" selection="multiple" v-model:selected="selectedPlayers">
           <template v-slot:top>
             <div class="text-h6 q-mr-md">Players</div>
             <q-input placeholder="search" outlined dense>
@@ -40,22 +40,17 @@
               </template>
             </q-input>
           </template>
-          <template v-slot:body-cell-action="props">
-            <q-td :props="props">
-              <q-radio v-model="selectedPlayers" color="red-8" dense />
-            </q-td>
-          </template>
         </q-table>
       </div>
       <div class="col col-md-6 col-xs-12 q-pa-xs">
         <div class="text-center text-h6 q-mb-md text-bold">Matches</div>
         <q-card class="bg-white q-mt-md">
           <q-list separator>
-            <q-item v-for="match in dummyMatchData" :key="match.matchNumber" clickable v-ripple>
+            <q-item v-for="(match, index) in dummyMatchData" :key="match.matchId" clickable v-ripple>
               <q-item-section>
-                <q-item-label overline>Match #{{ match.matchNumber }}</q-item-label>
-                <q-item-label class="text-body1">{{ match.players[0].player1 }}</q-item-label>
-                <q-item-label class="text-body1">{{ match.players[0].player2 }}</q-item-label>
+                <q-item-label overline>Match #{{ index + 1 }}</q-item-label>
+                <q-item-label class="text-body1">{{ match.players[0].player1.name }}</q-item-label>
+                <q-item-label class="text-body1">{{ match.players[0].player2.name }}</q-item-label>
               </q-item-section>
               <q-item-section avatar>
                 <q-btn color="blue-8" label="edit" />
@@ -64,21 +59,69 @@
           </q-list>
         </q-card>
       </div>
-      <div class="q-mt-md">
+      <div class="q-mt-md flex q-gutter-sm">
         <q-btn color="blue-8" @click="back" label="back" />
       </div>
-      <q-dialog>
-        <div>
-          Are you sure you're going to match these players?
-
-        </div>
+      <q-dialog v-model="pairDialog">
+        <q-card>
+          <q-card-section>
+            <div class="text-h6">
+              Pair Players?
+            </div>
+            <div>
+              Are you sure you're going to pair and match these players?
+            </div>
+          </q-card-section>
+          <q-card-actions align="right">
+            <q-btn flat @click="cancel" label="Cancel" color="primary" v-close-popup />
+            <q-btn flat @click="matchPlayers" label="Yes" color="primary" v-close-popup />
+          </q-card-actions>
+        </q-card>
       </q-dialog>
     </div>
   </q-page>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, watchEffect } from 'vue'
+import { uid } from 'quasar';
+
+const id = uid()
+
+const pairDialog = ref(false)
+const selectedPlayers = ref([])
+
+const matchPlayers = () => {
+  const newMatch = {
+    matchId: id,
+    players: [
+      {
+        player1: {
+          playerId: id,
+          name: selectedPlayers.value[0].name,
+        },
+        player2: {
+          playerId: id,
+          name: selectedPlayers.value[1].name,
+        },
+      },
+    ]
+  }
+  dummyMatchData.value.push(newMatch)
+  selectedPlayers.value = []
+}
+
+// trigger dialog if 2 players are selected.
+watchEffect(() => {
+  if (selectedPlayers.value.length === 2) {
+    pairDialog.value = true
+  }
+})
+
+const cancel = () => {
+  selectedPlayers.value = []
+}
+
 
 const selectedGender = ref('Boys')
 const selectedCategory = ref('Kids')
@@ -113,7 +156,6 @@ const columns = [
   { name: 'name', required: true, label: 'Name', align: 'left', field: 'name', sortable: true },
   { name: 'gender', align: 'center', label: 'Gender', field: 'gender', sortable: true },
   { name: 'weightClass', label: 'Weight Class', field: 'weightClass', sortable: true },
-  { name: 'action', label: 'Actions', align: 'center', sortable: false }
 ]
 const rows = [
   {
@@ -149,35 +191,53 @@ const rows = [
   },
 ]
 
-const dummyMatchData = [
+const dummyMatchData = ref([
   {
-    matchNumber: 1,
+    matchId: id,
     players: [
       {
-        player1: 'Justin Villacampa',
-        player2: 'John Vince Paderna',
+        player1: {
+          playerId: id,
+          name: 'Justin Villacampa'
+        },
+        player2: {
+          playerId: id,
+          name: 'John Vince Paderna'
+        },
       },
     ],
   },
   {
-    matchNumber: 2,
+    matchId: id,
     players: [
       {
-        player1: 'Sir Clint',
-        player2: 'Nami',
+        player1: {
+          playerId: id,
+          name: 'Sir Clint'
+        },
+        player2: {
+          playerId: id,
+          name: 'Nami Dog'
+        },
       },
     ],
   },
   {
-    matchNumber: 3,
+    matchId: id,
     players: [
       {
-        player1: 'Angel',
-        player2: 'Oreo',
+        player1: {
+          playerId: id,
+          name: 'Angel'
+        },
+        player2: {
+          playerId: id,
+          name: 'Oreo'
+        },
       },
     ],
   },
-];
+])
 
 
 </script>
