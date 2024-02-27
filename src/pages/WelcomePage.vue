ebcc118c04ac909652b476b3a2419ce1e7d203a0
tatin <tatenvader123@gmail.com>
2024-02-28 07:34:26 +0800

    updated ui on register form on welcome page

--- src/pages/WelcomePage.vue
+++ src/pages/WelcomePage.vue
@@ -33,7 +33,7 @@
       <q-card style="width: 400px">
         <q-form @submit="previewMember ? updateMember() : createMember()">
           <q-card-section>
-            <div class="text-bold text-h6 q-mb-md">Add Member</div>
+            <div class="text-bold text-h6 q-mb-md">Be a Member</div>
             <q-input v-model="form.lastName" class="q-mb-sm" label="Last Name" outlined dense :rules="[(val) => !!val]" />
             <q-input v-model="form.firstName" class="q-mb-sm" label="First Name" outlined dense
               :rules="[(val) => !!val]" />
@@ -79,7 +79,7 @@
                 <q-icon name="attach_file" />
               </template>
             </q-file>
-            <q-btn :loading="isLoading" type="submit" label="Submit" color="red-8" />
+            <q-btn :loading="isLoading" type="submit" label="Register" color="red-8" />
           </q-card-section>
         </q-form>
       </q-card>
